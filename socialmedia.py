3189e5140b3cf8b7b7fede1ba789de45942bcaa8
Wei Zhang <davidzhang.seu@gmail.com>
2013-06-17 15:52:56 -0700

    add twitter api support

--- socialmedia.py
+++ socialmedia.py
@@ -1,25 +1,75 @@
-import twitter
+#!/usr/bin/env python
+
+import datetime
+import sys
+import time
+import json
+from oauth_hook import OAuthHook
+import requests0 as requests
 
 import config
 
 
-twitter_api = twitter.Api(consumer_key = config.TWITTER_CONSUMER_KEY, 
-                          consumer_secret = config.TWITTER_CONSUMER_SECRET, 
-                          access_token_key = config.TWITTER_ACCESS_TOKEN, 
-                          access_token_secret = config.TWITTER_ACCESS_TOKEN_SECRET)
+twitter_oauth_hook = OAuthHook(access_token=config.TWITTER_ACCESS_TOKEN, 
+                       access_token_secret=config.TWITTER_ACCESS_TOKEN_SECRET, 
+                       consumer_key=config.TWITTER_CONSUMER_KEY, 
+                       consumer_secret=config.TWITTER_CONSUMER_SECRET, 
+                       header_auth=False)
 
-def twitter_search(query="twitter"):
-    tweets = twitter_api.GetSearch(query, count=100, lang='en')
-    for tweet in tweets:
-        print tweet.text
+def throttle_hook(response):
+    ratelimited = "x-ratelimit-remaining" in response.headers and \
+                  "x-ratelimit-reset" in response.headers 
 
-def twitter_user(query="facebook"):
-    """bulk user query, users/lookup
+    if ratelimited:
+        remaining = int(response.headers["x-ratelimit-remaining"])
+        reset = datetime.datetime.utcfromtimestamp(float(response.headers["x-ratelimit-reset"]))
+        now = datetime.datetime.utcnow()
+        
+        time_to_reset = reset - now
+        time_to_sleep = time_to_reset.seconds / remaining
+
+        sys.stderr.write("Throttling... Sleeping for %d secs...\n" % time_to_sleep)
+        time.sleep(time_to_sleep)
+
+class TwitterError(Exception):
+    """Base class for Twitter errors
     """
-    pass
+    @property
+    def message(self):
+        """Returns the first argument used to construct this error.
+        """
+        return self.args[0]
+
+def check_twitter_error(data):
+    if 'error' in data:
+        raise TwitterError(data['error'])
+    if 'errors' in data:
+        raise TwitterError(data['errors'])
+
+def search_tweets(query=None, count=10, lang='en', result_type='recent'):
+    client = requests.session(hooks={'pre_request': twitter_oauth_hook, 'response': throttle_hook})
+
+    request_url = "https://api.twitter.com/1.1/search/tweets.json?q=%s&lang=%s&count=%d&result_type=%s" % \
+                    (query, lang, count, result_type)
+    resp = client.get(request_url)
+    json_content = resp.content
+    try:
+        data = json.loads(json_content)
+        check_twitter_error(data)
+    except ValueError:
+        if "<title>Twitter / Over capacity</title>" in json_content:
+            raise TwitterError("Capacity Error")
+        if "<title>Twitter / Error</title>" in json_content:
+            raise TwitterError("Technical Error")
+        raise TwitterError("json decoding")
+
+    # parse json data
+    for x in data['statuses']:
+        print x
 
 def main():
-    twitter_search("iphone")
+    search_tweets("iphone")
 
 if __name__ == "__main__":
     main()
+
