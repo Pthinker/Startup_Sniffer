2ad844c5e2546ec4680a7e60587e5804f28e8eb5
Wei Zhang <davidzhang.seu@gmail.com>
2013-07-01 10:21:59 -0700

    add d3 chart for individual startup metrics

--- app/board/views.py
+++ app/board/views.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from app import db, startup_info, APP_STATIC
 from app.models import *
 import os
 import json
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+import json
 
 board_page = Blueprint('board', __name__, url_prefix='/board')
 
@@ -23,7 +24,7 @@ def index():
     # Get past metric data to compute trend
     past_records = db.session.query(StartupInfo, ALCompany).filter(
             StartupInfo.al_id==ALCompany.angellist_id).filter(
-            StartupInfo.info_date==(date.today()-timedelta(3))).all()
+            StartupInfo.info_date==(date.today()-timedelta(5))).all()
 
     # Find maximum number of each tracking metric
     data = {"angellist":0, "twitter":0, 'bitly':0}
@@ -75,10 +76,10 @@ def index():
         ranked_list[al_id]['data'] = [name, al_quality, al_follower, twitter, bitly, img]
 
         # trend
-        al_follow_trend = (al_follower - past_data[al_id][0]) / 3
-        al_quality_trend = (al_quality - past_data[al_id][1]) /3
-        twitter_trend = (twitter - past_data[al_id][2]) / 3
-        bitly_trend = (bitly - past_data[al_id][3]) / 3
+        al_follow_trend = (al_follower - past_data[al_id][0]) / 5
+        al_quality_trend = (al_quality - past_data[al_id][1]) /5
+        twitter_trend = (twitter - past_data[al_id][2]) / 5
+        bitly_trend = (bitly - past_data[al_id][3]) / 5
         trend_list[al_id] = {}
         trend_list[al_id]['score'] = al_follow_trend + al_quality_trend + \
                                      twitter_trend + bitly_trend
@@ -92,3 +93,30 @@ def index():
     return render_template("board/index.html", ranked_list=ranked_list, 
             trend_list=trend_list)
 
+@board_page.route('/startup')
+@board_page.route('/startup/<int:al_id>')
+def startup(al_id):
+    records = db.session.query(StartupInfo).filter(
+            StartupInfo.al_id==al_id).order_by(StartupInfo.info_date).all()
+
+    twitter_data = []
+    al_follower_data = []
+    al_quality_data = []
+    bitly_data = []
+
+    for record in records:
+        ts = int(record.info_date.strftime("%s")) * 1000
+        twitter_data.append([ts, record.twitter_follower])
+        al_follower_data.append([ts, record.al_follower])
+        al_quality_data.append([ts, record.al_quality])
+        bitly_data.append([ts, record.bitly_click])
+
+    data = [
+             {"key": "Twitter follower", "values": twitter_data},
+             {"key": "Angellist follower", "values": al_follower_data},
+             {"key": "Angellist quality", "values": al_quality_data},
+             {"key": "Bitly click", "values": bitly_data},
+    ];
+    
+    return render_template("board/startup.html", data=data)
+    
